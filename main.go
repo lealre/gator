d13838ffcb290c45b2eb7676b734c61bd5422a90
lealre <renaanvp@gmail.com>
2025-04-03 21:04:29 +0100

    Add command structures, starting with `login`

--- main.go
+++ main.go
@@ -2,22 +2,79 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/lealre/gator/internal/config"
 )
 
 func main() {
-	name := "renan"
 	cfg, err := config.Read()
 	if err != nil {
 		fmt.Print(err)
 	}
+	s := &state{config: &cfg}
+	cmd := &commands{commands: make(map[string]func(*state, command) error)}
+	cmd.register("login", handlerLogin)
 
-	err = cfg.SetUser(name)
-	if err != nil {
-		fmt.Print(err)
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide a command")
+		os.Exit(1)
 	}
-	cfg, _ = config.Read()
 
-	fmt.Printf("%+v\n", cfg)
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: login <name>")
+		os.Exit(1)
+	}
+	userCmd := os.Args[1]
+	args := os.Args[2:]
+
+	command := command{name: userCmd, args: args}
+	err = cmd.run(s, command)
+	if err != nil {
+		errorMessage := fmt.Errorf("error executing command %s: %w", userCmd, err)
+		fmt.Println(errorMessage)
+		os.Exit(1)
+	}
+
+}
+
+type state struct {
+	config *config.Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("missing username")
+	}
+
+	userName := cmd.args[0]
+	err := s.config.SetUser(userName)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("User setted as %s\n", userName)
+	return nil
+}
+
+type commands struct {
+	commands map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.commands[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	if f, ok := c.commands[cmd.name]; ok {
+		f(s, command{name: cmd.name, args: cmd.args})
+	} else {
+		fmt.Printf("Command %s not found\n", cmd.name)
+	}
+	return nil
 }
