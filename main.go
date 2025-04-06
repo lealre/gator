9f152f62ee024f4205c3a9041d4abf6abcc3bd4c
lealre <renaanvp@gmail.com>
2025-04-06 21:19:44 +0100

    Start `agg` command

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"database/sql"
+	"encoding/xml"
 	"fmt"
+	"html"
+	"io"
+	"net/http"
 	"os"
 	"time"
 
@@ -33,6 +37,7 @@ func main() {
 	cmd.register("register", handlerRegister)
 	cmd.register("reset", handlerReset)
 	cmd.register("users", handlerListUsers)
+	cmd.register("agg", hendlerAggregate)
 
 	if len(os.Args) < 2 {
 		fmt.Println("Please provide a command")
@@ -184,5 +189,76 @@ func (c *commands) run(s *state, cmd command) error {
 	} else {
 		fmt.Printf("Command %s not found\n", cmd.name)
 	}
+	return nil
+}
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := http.Client{Timeout: 10 * time.Second}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error requesting %s: %w", feedUrl, err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var feedData RSSFeed
+	err = xml.Unmarshal(body, &feedData)
+	if err != nil {
+		return nil, err
+	}
+
+	decodeHTMLEntities(&feedData)
+
+	return &feedData, nil
+}
+
+func decodeHTMLEntities(feed *RSSFeed) {
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+
+	for i := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(feed.Channel.Item[i].Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(feed.Channel.Item[i].Description)
+	}
+}
+
+func hendlerAggregate(s *state, cmd command) error {
+	url := "https://www.wagslane.dev/index.xml"
+	ctx := context.Background()
+
+	data, err := fetchFeed(ctx, url)
+	if err != nil {
+		return err
+	}
+
+	fmt.Print(data)
 	return nil
 }
